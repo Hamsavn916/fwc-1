f0dc9be85b5e212c4a68b06f26e4b5045bffe2ba
Hamsavn916 <hamsavn.ec22@bmsce.ac.in>
2025-03-13 16:39:23 +0530

    Create gvv_ard_7447.ino

---
+++ ide/7447/codes/gvv_ard_7447/gvv_ard_7447.ino
@@ -0,0 +1,114 @@
+#include <Arduino.h>
+
+int D,C,B,A;
+
+void disp_7447(int D,int C,int B, int A)
+{
+  digitalWrite(2, A); //LSB
+  digitalWrite(3, B); 
+  digitalWrite(4, C); 
+  digitalWrite(5, D); //MSB
+}
+
+void setup() {
+ 
+pinMode(2, OUTPUT);  
+pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(5, OUTPUT);
+pinMode(6, INPUT);
+pinMode(7, INPUT);
+pinMode(8, INPUT);
+pinMode(9, INPUT);
+pinMode(13, OUTPUT);
+
+}
+
+void loop() {
+ 
+ digitalWrite(13, HIGH);
+        delay(1000);
+
+ disp_7447(0,0,0,0);
+
+ digitalWrite(13, LOW);
+        delay(100);
+
+
+   digitalWrite(13, HIGH);
+        delay(1000);
+
+ disp_7447(0,0,0,1);
+ 
+ digitalWrite(13, LOW);
+        delay(100);
+
+
+  digitalWrite(13, HIGH);
+        delay(1000);
+
+ disp_7447(0,0,1,0);
+ 
+ digitalWrite(13, LOW);
+        delay(100);
+
+
+  digitalWrite(13, HIGH);
+        delay(1000);
+
+ disp_7447(0,0,1,1);
+ 
+ digitalWrite(13, LOW);
+        delay(100);
+
+
+ digitalWrite(13, HIGH);
+        delay(1000);
+
+ disp_7447(0,1,0,0);
+ 
+ digitalWrite(13, LOW);
+        delay(100);
+  
+
+  digitalWrite(13, HIGH);
+        delay(1000);
+
+ disp_7447(0,1,0,1);
+ 
+ digitalWrite(13, LOW);
+        delay(100);
+  
+  digitalWrite(13, HIGH);
+        delay(1000);
+
+ disp_7447(0,1,1,0);
+ 
+ digitalWrite(13, LOW);
+        delay(100);
+  
+  digitalWrite(13, HIGH);
+        delay(1000);
+
+ disp_7447(0,1,1,1);
+ 
+ digitalWrite(13, LOW);
+        delay(100);
+  
+  digitalWrite(13, HIGH);
+        delay(1000);
+
+ disp_7447(1,0,0,0);
+ 
+ digitalWrite(13, LOW);
+        delay(100);
+
+  digitalWrite(13, HIGH);
+        delay(1000);
+
+ disp_7447(1,0,0,1);
+ 
+ digitalWrite(13, LOW);
+        delay(100);
+
+}
